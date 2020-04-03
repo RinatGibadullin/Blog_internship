6012e16c43a50a76756a2921d1d5458aa05011a2
RinatGibadullin <Rinat990104@yandex.ru>
2020-04-03 15:09:59 +0300

    fixed all

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable, :lockable
   validates :full_name, presence: true
-  has_many :articles, :dependent => :destroy
+  has_many :articles, dependent: :destroy
 end
